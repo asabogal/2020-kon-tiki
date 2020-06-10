7e5d39acb563f6f8f258308f98fc556ab7299b0a
asabogal <asabogal.git@gmail.com>
2020-06-09 21:20:58 -0400

    init App with Helmet and initial router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Helmet from 'react-helmet';
+import LandingPage from './pages/LandingPage';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Helmet>
+        <title>Kon-Tiki Greenport</title>
+        <meta name="description" content="Kon-Tiki Restaurant and Bar at The Gallery Hotel" />
+        <meta name="keywords" content="kon-tiki, kon, tiki, greenport, restaurant, bar, gallery hotel, gallery, hotel" />
+      </Helmet>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={LandingPage}/>
+        </Switch>
+      </BrowserRouter>
+    </>
   )
 }
 
