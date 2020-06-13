b62c6c34650e66cb8ea7ecad10782219df7b2a02
asabogal <asabogal.git@gmail.com>
2020-06-13 11:15:03 -0400

    change header info and style

--- src/components/navigation/NavHeader.js
+++ src/components/navigation/NavHeader.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
-import Logo from '../utils/Logo';
-import {MenuButton} from '../utils/Buttons';
+import SocialBar from '../utils/SocialBar';
 
 const NavHeader = () => {
   return (
     <Container>
-      <LogoContainer>
-        <Logo/>
-      </LogoContainer>
-      <MenuBtnContainer>
-        <MenuButton dark/>
-      </MenuBtnContainer>
+        <a href="tel: +1-631-477-4007">631-477-4007</a>
+        <Hours>
+          <p>Fri, Sat, Sun</p>
+          <p>12-8pm</p>
+        </Hours>
+        <SocialBar
+          color='white'
+          size='lg'
+        />
     </Container>
   );
 };
@@ -21,20 +23,60 @@ const Container = styled.div`
   position: sticky;
   top: 0;
   display: flex;
-  justify-content: space-between;
-  background-color: black;
+  justify-content: space-around;
   align-items: center;
   z-index: 900;
-  width: 100vw;
-  height: 90px;
+  height: 50px;
+  padding: 0px 40px 10px 40px;
+  background-color: black;
+  z-index: 900;
+  a {
+    text-decoration: none;
+    color: white;
+    font-size: 14px;  
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+  @media (max-width: 650px) {
+    padding: 0px 10px 30px 10px;
+  }
+`;
+
+const Hours = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  p {
+    padding: 0;
+    margin: 0;
+    text-align: center;
+    justify-items: center;
+    font-size: 14px;
+  }
 `;
 
 const LogoContainer = styled.div`
   padding-left: 40px;
 `;
 
-const MenuBtnContainer = styled.div`
+const InfoContainer = styled.div`
   display: flex;
   align-items: center;
   padding-right: 40px;
+  a {
+    text-decoration: none;
+    color: white;
+    font-size: 14px;
+    padding-right: 5px;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `;
+
+// const MenuBtnContainer = styled.div`
+//   display: flex;
+//   align-items: center;
+//   padding-right: 40px;
+// `;
