7137c311c57a87a13f7979c9421a55edaf852043
asabogal <asabogal.git@gmail.com>
2020-06-18 19:02:42 -0400

    add strong font to chef cheo, adjust height for media

--- src/components/main/Announcement.js
+++ src/components/main/Announcement.js
@@ -5,9 +5,9 @@ const Announcement = () => {
   return (
     <Container>
       <h1>WE ARE OPEN!!</h1>
-      <h2>Visit us for full-service outdoor seating or window pick up</h2>
+      <h2>Visit us for full-service, outdoor seating or window pick up</h2>
       <p>KonTiki is one of the North Fork’s most intriguing restaurants and bars.</p>
-      <p>Spearheaded by Chef Cheo, our unique approach to cusine sets us apart from any culinary experience in the area.</p>
+      <p>Spearheaded by <strong style={{fontSize: '22px'}}>Chef Cheo</strong>, our unique approach to cusine sets us apart from any culinary experience in the area.</p>
       <p>Located at the beautiful Gallery Hotel in Greenport, NY, we welcome you to join us and experience it for yourself!</p>
     </Container>
   );
@@ -29,6 +29,6 @@ const Container = styled.div`
     margin-block-end: 3px;
   }
   @media (max-width: 999px) {
-    height: 90vh;
+    height: 100vh;
   }
 `;
