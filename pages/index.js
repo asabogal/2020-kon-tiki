f54d6c1f3c9cd4136fabbf09710b5e01d8c39cd3
asabogal <asabogal.git@gmail.com>
2020-11-21 16:19:57 -0500

    init app with nextjs and styled components scaffold

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const Home = () => {
+  return  (
+    <Title>
+      NEXT JS
+    </Title>
+  )
+}
+
+export default Home;
+
+
+const Title = styled.h1`
+  font-size: 50px;
+`;
