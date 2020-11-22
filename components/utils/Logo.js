302db985fb0059d5a110a6122d58644ecb1c8629
asabogal <asabogal.git@gmail.com>
2020-11-21 20:07:19 -0500

    migrate Logo

---
+++ components/utils/Logo.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+import logo from '../../public/images/navlogo.png';
+
+const Logo = () => {
+  return (
+    <Container>
+      <img src={logo} alt='kontiki-logo'/>
+    </Container>
+  );
+};
+
+export default Logo;
+
+const Container = styled.div`
+  width: 90px;
+  height: 90px;
+  border-radius: 50%;
+  padding: 18px;
+  border: 2px solid white;
+  @media (max-width: 768px) {
+    width: 75px;
+    height: 75px;
+  }
+  img {
+    height: 100%;
+    width: 100%;
+  }
+`;
