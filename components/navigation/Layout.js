7175b572a3da2987a7fb80cf2a1bb67fedc08633
asabogal <asabogal.git@gmail.com>
2020-11-21 19:10:51 -0500

    migrate navigation components

---
+++ components/navigation/Layout.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+import NavHeader from './NavHeader';
+
+const Layout = ({children}) => {
+  return (
+    <>
+    <Container>
+      <NavHeader/>
+        {children}
+    </Container>
+    </>
+  );
+};
+
+export default Layout;
+
+const Container = styled.div`
+  height: 100%;
+`;
