47b119510bcf5517c60f2662cc4806c866932b63
Alejandro Sabogal <asabogal.dev@gmail.com>
2020-12-03 18:12:18 -0500

    init admin components with Login

---
+++ components/admin/Login.js
@@ -0,0 +1,84 @@
+import React, {useReducer, useState, useEffect} from 'react';
+import styled from 'styled-components';
+
+const Login = () => {
+
+  const [userInput, setUserInput] = useReducer((state, newState) => ({...state, ...newState}),
+  {
+    user: '',
+    password: ''
+  }  
+);
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setUserInput({
+      [name]: value
+    });
+  };
+
+ 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert('submitted');
+  };
+
+  return (
+    <FormContainer>
+      <h1>Admin Login</h1>
+      <form onSubmit={handleSubmit}>
+      <input
+        placeholder="user"
+        type="text"
+        name="user"
+        value={userInput.value}
+        onChange={handleChange}
+      />
+      <input
+        placeholder="password"
+        type="password"
+        name="password"
+        value={userInput.value}
+        onChange={handleChange}
+      />
+      <Button type='submit'>
+        Submit
+      </Button>
+      </form>
+    </FormContainer>
+  );
+};
+
+export default Login;
+
+const FormContainer = styled.div`
+  margin: 10vh auto;
+  display: grid;
+  justify-items: center;
+  justify-content: center;
+  align-content: center;
+  form {
+    width: 300px;
+    display: grid;
+    grid-gap: 10px
+  }
+  input {
+    border: 1px thin black;
+    height: 20px;
+    font-size: 1rem;
+    padding: 5px;
+  }
+  a {
+    &:hover {
+      font-weight: bold;
+    }
+  }
+`;
+
+const Button = styled.button`
+  width: 100%;
+  height: 5vh;
+  background-color: black;
+  color: white;
+  cursor: pointer;
+`;
