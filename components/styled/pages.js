2cb3cf08fe0a10d742e5407ff942308cf52417f9
Alejandro Sabogal <asabogal.dev@gmail.com>
2020-12-03 18:50:19 -0500

    init styled component folder with pages

---
+++ components/styled/pages.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const AdminPageContainer = styled.div`
+  position: fixed;
+  top: 0;
+  height: 100vh;
+  width: 100vw;
+  background-color: #191b30;
+`;
