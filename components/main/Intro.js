5dd9f6f957e87c84cc3023a29bd2e58ddad3308d
Alejandro Sabogal <asabogal.dev@gmail.com>
2020-12-02 19:54:56 -0500

    change text + fix text margins

--- components/main/Intro.js
+++ components/main/Intro.js
@@ -4,7 +4,6 @@ const Intro = () => {
   return (
     <Container>
       <h1>WELCOME!</h1>
-      <h2>We are open for indoor and outdoor seating, and pickup orders.</h2>
       <p>KonTiki is one of the North Fork’s most intriguing restaurants and bars.</p>
       <p>Spearheaded by <strong style={{fontSize: '22px'}}>Chef Cheo</strong>, our unique approach to cusine sets us apart from any culinary experience in the area.</p>
       <p>Located at the beautiful Gallery Hotel in Greenport, NY, we welcome you to join us and experience it for yourself!</p>
@@ -15,7 +14,6 @@ const Intro = () => {
 export default Intro;
 
 const Container = styled.div`
-  display: grid;
   height: 45vh; 
   text-align: center;
   margin-bottom: 5vh;
@@ -23,14 +21,14 @@ const Container = styled.div`
   justify-content: center;
   margin: 0 auto;
   h1 {
-    margin-block-end: 10px;
+    margin-block-end: 5vh;
   }
   p {
     font-size: 20px;
-    margin-block-start: 3px;
-    margin-block-end: 3px;
+    margin-block-start: 2vh;
+    margin-block-end: 2vh;
   }
   @media (max-width: 499px) {
-    height: 80vh;
+    height: 55vh;
   }
 `;
