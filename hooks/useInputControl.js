bf5a470333f433d30b1cfd5a2b0d2b1e39877d8a
Alejandro Sabogal <asabogal.dev@gmail.com>
2020-12-08 13:49:33 -0500

    add textInput validation

--- hooks/useInputControl.js
+++ hooks/useInputControl.js
@@ -1,4 +1,4 @@
-import {useReducer} from 'react';
+import {useReducer, useState} from 'react';
 
 const useInputcontrol = () => {
 
@@ -6,18 +6,68 @@ const useInputcontrol = () => {
     {}  
   );
 
+  const [inputErrors, setInputErrors] = useState({})
+
   const handleChange = (e) => {
     const {name, value} = e.target;
     setUserInput({
       [name]: value
     });
-  }
+  
+    if (!inputErrors.name) {
+      setInputErrors({
+        ...inputErrors,
+        [name]: {
+          isValid: true,
+          errors: []
+        }
+      })
+    }
+  };
 
   const reset = () => {
-   setUserInput("");
+    setUserInput("");
+   };
+
+// validations
+  const TEXT_VALIDATORS = [
+    {
+      type: 'require',
+      error: "can't be blank."
+    },
+    {
+      type: 'minLength',
+      error: 'must be at least 3 characters long.'
+    }
+  ];
+
+  const validateTextInput = (e) => {
+    const {name, value} = e.target;
+
+    for (const validator of TEXT_VALIDATORS) {
+      if (validator.type === 'require' && value.trim().length <= 0) {
+        setInputErrors({
+          ...inputErrors,
+          [name]: {
+            isValid: false,
+            errors: [`${name} ${validator.error}`]
+          }
+        })
+        return;
+      }
+      if (validator.type === 'minLength' && value.trim().length <= 3) {
+        setInputErrors({
+          [name]: {
+            isValid: false,
+            errors: [`${name} ${validator.error}`]
+          }
+        })
+      }   
+    }
   }
 
-  return [userInput, handleChange, reset];
+  
+  return [userInput, handleChange, reset, inputErrors, validateTextInput];
 };
 
 export default useInputcontrol;
