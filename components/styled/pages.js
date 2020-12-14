fd2b16d0ae61d084865a783750997418d36b485b
Alejandro Sabogal <asabogal.dev@gmail.com>
2020-12-13 20:59:52 -0500

    add MenuPages styles

--- components/styled/pages.js
+++ components/styled/pages.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const AdminPageContainer = styled.div`
+export const LoginPageContainer = styled.div`
   position: fixed;
   top: 0;
   height: 100vh;
@@ -15,4 +15,22 @@ export const DashboardContainer = styled.div`
 
 export const DashboardContent = styled.div`
   display: flex;
+`;
+
+export const MenusPageContainer = styled.div`
+  width: 100vw;
+  min-height: 100vh;
+  background-color: #191b30;
+`;
+
+export const MenuFormContainer = styled.div`
+  margin: 0 auto;
+  width: 50vw;
+  display: flex;
+  flex-direction: column;
+  h2 {
+    color: white;
+    text-align: center;
+    margin-block-end: 0px;
+  }
 `;
