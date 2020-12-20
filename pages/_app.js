dd5128239bd1e2f5eaa1044d6d55d0af7f6d6f28
Alejandro Sabogal <asabogal.dev@gmail.com>
2020-12-19 21:30:44 -0500

    init auth logic with auth context

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
-import {useRouter} from 'next/router';
-import {createGlobalStyle} from 'styled-components'
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { createGlobalStyle } from 'styled-components'
 import Layout from '../components/navigation/Layout';
 import Helmet from 'react-helmet';
+// context
+import AuthContext from '../context/auth';
 
 const GlobalStyle = createGlobalStyle`
 
@@ -27,28 +30,47 @@ h2 {
 }
 `;
 
- const App = ({ Component, pageProps }) => {
-   
+const App = ({ Component, pageProps }) => {
+
   const router = useRouter();
   const adminPath = router.pathname.split('/')[1];
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const authenticated = window.localStorage.getItem('isLoggedIn');
+    if (authenticated === 'true')  login();
+    if (!authenticated === 'false')  logout();
+  }, []);
+
+  const login = () => {window.
+    window.localStorage.setItem('isLoggedIn', true);
+    setIsLoggedIn(true);
+  };
+
+  const logout = () => {
+    window.localStorage.setItem('isLoggedIn', false);
+    setIsLoggedIn(false);
+  };
 
   return (
-    <> 
+    <>
       <Helmet>
         <title>Kon-Tiki Greenport</title>
         <meta name="description" content="Kon-Tiki Restaurant and Bar at The Gallery Hotel" />
         <meta name="keywords" content="kon-tiki, kontiki, kontiki-gp, kontiki greenport, kon-tiki grenport, kon, tiki, greenport, restaurant, bar, gallery hotel, gallery, hotel" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&family=Yanone+Kaffeesatz:wght@200;300;400;500;600;700&display=swap" rel="stylesheet"/>           
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&family=Yanone+Kaffeesatz:wght@200;300;400;500;600;700&display=swap" rel="stylesheet" />
       </Helmet>
       <GlobalStyle />
-      {
-        adminPath !== 'admin' ?
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      :
-        <Component {...pageProps} />
-      }
+      <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+        {
+          adminPath !== 'admin' ?
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+            :
+            <Component {...pageProps} />
+        }
+      </AuthContext.Provider>
     </>
   )
 }
