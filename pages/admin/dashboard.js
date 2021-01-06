1d05e07cbf8498353e2509a2e6fb7e6a98ecbcd3
Alejandro Sabogal <asabogal.dev@gmail.com>
2021-01-06 18:45:54 -0500

    protect page with localStorage

--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -1,20 +1,36 @@
+import { useContext, useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import useLocalStorage from '../../hooks/useLocalStorage';
 // components
 import NavHeader from '../../components/admin/dashboard/NavHeader';
 import SideBar from '../../components/admin/dashboard/SideBar';
 import NewReservationForm from '../../components/admin/dashboard/NewReservationForm';
+import AccessDenied from '../../components/utils/AccessDenied';
 // styled 
-import {LoginPageContainer, DashboardContainer, DashboardContent} from '../../components/styled/pages';
+import { LoginPageContainer, DashboardContainer, DashboardContent } from '../../components/styled/pages';
 
 const Dashboard = () => {
+  const [authenticated, setIsAuthenticated] = useLocalStorage("isLoggedIn", true);
+  const [authorized, setAuthorized] = useState(null);
+
+  useEffect(() => {
+    authenticated ? setAuthorized(true) : setAuthorized(false);
+  }, [authenticated]);
+ 
   return (
     <LoginPageContainer>
-      <DashboardContainer>
-        <NavHeader/>
+    { authorized ? 
+      <DashboardContainer >
+        <NavHeader />
         <DashboardContent>
-          <SideBar/>
-          <NewReservationForm/>
+          <SideBar />
+          <NewReservationForm />
         </DashboardContent>
       </DashboardContainer>
+    :
+      <AccessDenied/>
+    }
+
     </LoginPageContainer>
   );
 };
