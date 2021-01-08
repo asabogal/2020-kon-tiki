8303147e9fdcdc744994994941488ce41fa50883
Alejandro Sabogal <asabogal.dev@gmail.com>
2021-01-08 18:54:03 -0500

    add reservations dummy data

---
+++ dummyData/Reservations.js
@@ -0,0 +1,29 @@
+export const reservationsData = [
+  {
+    id: 1,
+    name: 'John Smith',
+    phone: '1234567890',
+    date: '2021-05-29T20:30',
+    guests: 3,
+    seating: 'Outdoor',
+    notes: ''
+  },
+  {
+    id: 2,
+    name: 'Jane Doe',
+    phone: '1234567890',
+    date: '2021-05-30T18:00',
+    guests: 5,
+    seating: 'Indoor',
+    notes: 'Dairy Allergy'
+  },
+  {
+    id: 3,
+    name: 'Bill Hamilton',
+    phone: '1231234567',
+    date: '2021-05-30T21:00',
+    guests: 8,
+    seating: 'Outdoor',
+    notes: "Friend's Birthday"
+  }
+]
